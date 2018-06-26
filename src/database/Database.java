3727382139bc1d788c76e7cc17717c565ad30148
Bruno R. Santos <bruesmanbruesman@hotmail.com>
2018-06-26 19:39:37 -0300

    updated database class for updating and deleting operations

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,5 +1,6 @@
 package database;
 import java.io.*;
+import java.util.List;
 
 import estudante.Alunos;
 
@@ -9,27 +10,45 @@ public class Database {
 	private String NOVA_LINHA = "\n";
 	private String FILE_NAME = "database.csv";
 	FileWriter writer;
-	FileReader reader;
 	
-	public void salvarAluno(Alunos aluno) {
+	public void salvar(List<Alunos> alunos) {
 		
-		String nome = aluno.getNome();
-		String email = aluno.getEmail();
-		Float renda = aluno.getRenda();
+		this.esvaziaBanco();
 		
+		for(Alunos aluno : alunos) {
+			
+			String nome = aluno.getNome();
+			String email = aluno.getEmail();
+			Float renda = aluno.getRenda();
+			
+			try {
+				
+				this.writer = new FileWriter(FILE_NAME, true);
+				this.writer.append(NOVA_LINHA);
+				this.writer.append(nome);
+				this.writer.append(DELIMITADOR);
+				this.writer.append(email);
+				this.writer.append(DELIMITADOR);
+				this.writer.append(String.valueOf(renda));
+				this.writer.append(DELIMITADOR);
+				this.writer.flush();
+				this.writer.close();
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void esvaziaBanco() {
+		File file = new File(FILE_NAME);
 		try {
 			
-			this.writer = new FileWriter(FILE_NAME, true);
-			
-			this.writer.append(NOVA_LINHA);
-			this.writer.append(nome);
-			this.writer.append(DELIMITADOR);
-			this.writer.append(email);
-			this.writer.append(DELIMITADOR);
-			this.writer.append(String.valueOf(renda));
-			this.writer.append(DELIMITADOR);
-			this.writer.flush();
-			this.writer.close();
+			file.createNewFile();
+			this.writer = new FileWriter(FILE_NAME, false);
+			this.writer.append("NOME,EMAIL,REDA");
+			writer.flush();
+			writer.close();
 			
 		} catch (IOException e) {
 			e.printStackTrace();
